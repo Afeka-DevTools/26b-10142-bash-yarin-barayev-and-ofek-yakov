a4f4fe494231fa5bc5dcb5ef3453e0cdf5fd4688
Yarin Barayev <Yarin.Barayev@s.afeka.co.il>
2026-06-22 19:37:21 +0300

    Added user_info script

---
+++ scripts/user_info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "User Information Script"
+echo "------------------------"
+echo "Username: $(whoami)"
+echo "Home Directory: $HOME"
+echo "Groups: $(groups)"
+echo "Default Shell: $SHELL"
+echo "-------------------------"
